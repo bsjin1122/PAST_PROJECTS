bc16a8cb952db5b2c2a5cbdeadc4f851ad78ef94
Jane <kitty7579@hanmail.net>
2022-06-23 00:44:52 +0900

    010 문자와 숫자

---
+++ es6/01-10.js
@@ -0,0 +1,11 @@
+// 001-009까지 es5 작성하면서 vscode에 문제가 생겨 삭제하고 이후부터 올립니다..!
+
+// 010 숫자와 문자
+const string10 = '10';
+const string20 = '20';
+const number10 = 10;
+const number20 = 20;
+
+console.log('string: %s', string10 + string20); //1020
+console.log('number: %d', number10 + number20); //30
+console.log('string + number : %s', string10 + number20); //문자 1020

99494cbd2a4e341449a6cdd04c33af6cc820e41b
Jane <kitty7579@hanmail.net>
2022-06-23 10:45:35 +0900

    040 new Date()

--- es6/030-040.js
+++ es6/030-040.js
@@ -25,3 +25,28 @@ listUser.forEach((user)=>{
     console.log(user);
 })
 console.log('--------------------');
+
+
+
+
+// 036 while 
+const number = 9;
+let value = 1;
+while (value < 10) {
+    console.log(number * value);
+    value +=1 ;
+}
+/* for: 조건식, 반복을 위한 변수의 초기값과 증감조건을 한눈에 볼 수 있어
+ 정해진 만큼 반복되는 코드를 작성할 때 쓰인다.
+ 
+ while: 조건식만 나타냄으로써 반복 그 자체에 의미를 두는 코드를 작성 */
+
+
+
+
+// 037 날짜 시간 생성하기
+const today = new Date();
+const date1 = new Date(2022, 9-1, 18, 20,30); //year, month, day, hour, min
+// month 0-11 
+console.log(today.toLocaleString());
+console.log(date1.toLocaleString());

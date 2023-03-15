affc22edb43694fcf42f5a4bc5d0affd228ef724
bsjin1122 <kitty7579@hanmail.net>
2023-03-16 08:47:57 +0900

    예외처리 학습2

--- exeception/error.py
+++ exeception/error.py
@@ -1,7 +1,14 @@
+"""
 try:
-    # 4 / 0
-    a = [1, 2, 3]
-    a[4]
-# except ZeroDivisionError as e:
-except IndexError as e:
+    a=[1, 2]
+    print(a[3])
+    4 / 0
+except (ZeroDivisionError,ZeroDivisionError) as e:
     print(e)
+"""
+
+# 오류 회피하기
+try:
+    f=open("나없는 파일", 'r')
+except FileNotFoundError:
+    pass

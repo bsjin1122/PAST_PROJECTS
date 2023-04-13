ddce6b26c293e46b61b1c5860ff044ebd7f0bc23
bsjin1122 <kitty7579@hanmail.net>
2023-04-13 18:14:43 +0900

    sbb 첫 커밋

--- src/main/java/com/mysite/sbb/HelloController.java
+++ src/main/java/com/mysite/sbb/HelloController.java
@@ -4,11 +4,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+// 컨트롤러 : 서버에 전달된 클라이언트의 요청(url과 전달된 파라미터 등) 처리하는 자바 클래스
 @Controller
 public class HelloController {
     @GetMapping("/hello")
-    @ResponseBody
-    public String hello() {
-        return "Hello World";
+    @ResponseBody // hello 메서드의 응답 결과가 문자열 그 자체임을 나타냄
+    public String hello(){
+        return "Hello SBB~~~!!";
     }
+
+
 }
